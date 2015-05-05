1345bdcdb29ccf3f7c698aa1b1206c18e5c7b26e
Simon Oulevay <simon.oulevay@gmail.com>
2015-05-05 18:48:21 +0200

    Added grunt-bump. Version bump to 0.1.1.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,21 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json'],
+        commit: false,
+        createTag: false,
+        push: false
+      }
+    },
+
     jshint: {
       files: [ 'Gruntfile.js', 'lib/**/*.js' ]
     }
   });
 
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('default', ['jshint']);
